6bbfa5f65dcea0b200aaa8b1aaee2f12c1f2ede0
arakeman <arakeman@outlook.com>
2017-04-19 18:38:41 -0700

    basics

---
+++ db/migrate/20170418200642_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :events
+
+    create_table :events do |t|
+      t.string :title
+      t.string :imageURL
+      t.text :description
+      t.datetime :date
+      t.string :tag
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
