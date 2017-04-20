da59fb817abd1208aa4d2ebae456c6bfd52f11b8
arakeman <arakeman@outlook.com>
2017-04-19 19:04:24 -0700

    homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :events
   get 'tag/:name', to: 'events#tag'
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
